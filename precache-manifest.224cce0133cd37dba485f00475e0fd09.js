f571b9f8805610bb9826b9c4b5a73cfd978e49b1
AmandeepSsaini <amandeepsingh.saini@hotmail.com>
2020-08-06 14:44:58 -0700

    Updates

---
+++ precache-manifest.224cce0133cd37dba485f00475e0fd09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39de529ded5237d1e871f6334866007d",
+    "url": "/AmandeepSsaini.github.io/catch-the-eye-react/index.html"
+  },
+  {
+    "revision": "9e75fc1bfaa3f4159929",
+    "url": "/AmandeepSsaini.github.io/catch-the-eye-react/static/css/main.154fdd86.chunk.css"
+  },
+  {
+    "revision": "b08a7c7106553a82db5c",
+    "url": "/AmandeepSsaini.github.io/catch-the-eye-react/static/js/2.e6de5ad5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AmandeepSsaini.github.io/catch-the-eye-react/static/js/2.e6de5ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e75fc1bfaa3f4159929",
+    "url": "/AmandeepSsaini.github.io/catch-the-eye-react/static/js/main.00cb1633.chunk.js"
+  },
+  {
+    "revision": "34f613660b45fc1b5bc0",
+    "url": "/AmandeepSsaini.github.io/catch-the-eye-react/static/js/runtime-main.cdfdc30b.js"
+  }
+]);
